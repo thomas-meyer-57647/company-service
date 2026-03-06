8c758d5f14f169063e87fc5b6c4bca21d9ebede2
Thomas Meyer <thomas.meyer.57647@gmail.com>
2026-03-06 02:09:49 +0100

    GET /companies (Liste) inkl. Paging/Sort

--- src/main/java/de/innologic/companyservice/persistence/repository/CompanyRepository.java
+++ src/main/java/de/innologic/companyservice/persistence/repository/CompanyRepository.java
@@ -2,9 +2,13 @@ package de.innologic.companyservice.persistence.repository;
 
 import de.innologic.companyservice.persistence.entity.CompanyEntity;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<CompanyEntity, String> {
 
     Optional<CompanyEntity> findByCompanyIdAndTrashedAtIsNull(String companyId);
+
+    Page<CompanyEntity> findAllByTrashedAtIsNull(Pageable pageable);
 }
